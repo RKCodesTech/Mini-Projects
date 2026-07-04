8759fb1e65faa8ac8ae5f773ebd0951a761e6e19
RKCodesTech <riyakosta2609@gmail.com>
2026-07-05 01:26:48 +0530

    d

--- StudentManagemen_Basic/src/main/java/in/sp/config/SpringConfigFile.java
+++ StudentManagemen_Basic/src/main/java/in/sp/config/SpringConfigFile.java
@@ -1,10 +1,27 @@
 package in.sp.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class SpringConfigFile {
 @Bean
-public 
+public DriverManagerDataSource myDatasource() {
+	DriverManagerDataSource drivermanager=new DriverManagerDataSource();
+	drivermanager.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	drivermanager.setUrl("jdbc:mysql://localhost:3306/spring_jdbc_db");
+	drivermanager.setUsername("root");
+	drivermanager.setPassword("1234");
+	return drivermanager;
+}
+@Bean
+public JdbcTemplate jdbctemplate() {
+	JdbcTemplate myjdbcteplate=new JdbcTemplate();
+	myjdbcteplate.setDataSource(myDatasource());
+	return myjdbcteplate;
+}
 }
