c1854b4c4bc328633c5db5c804d84237140dbd43
RKCodesTech <riyakosta2609@gmail.com>
2026-07-05 01:40:00 +0530

    k

--- StudentManagemen_Basic/src/main/java/in/sp/dao/StudentDAO.java
+++ StudentManagemen_Basic/src/main/java/in/sp/dao/StudentDAO.java
@@ -2,9 +2,15 @@ package in.sp.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import in.sp.beans.Student;
+
 public class StudentDAO {
 private JdbcTemplate jdbcTemplate;
 public  StudentDAO(JdbcTemplate jdbcTemplate){
 	this.jdbcTemplate=jdbcTemplate;
+}
+public int insertStudent(Student student) {
+	String sql= "INSERT INTO STUDENT VALUES(?,?,?)";
+	return jdbcTemplate.update(sql,student.getStd_rollno() ,student.getStd_name(),student.getStd_marks());
 }
 }
