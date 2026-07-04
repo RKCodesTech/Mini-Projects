b9614c18345d9515d6c6e7953608907ebfb219e5
RKCodesTech <riyakosta2609@gmail.com>
2026-07-04 23:26:18 +0530

    m

--- StudentManagemen_Basic/src/main/java/in/sp/beans/Student.java
+++ StudentManagemen_Basic/src/main/java/in/sp/beans/Student.java
@@ -1,5 +1,26 @@
 package in.sp.beans;
 
 public class Student {
+private int std_rollno;
+private String std_name;
+private int std_marks;
+public int getStd_rollno() {
+	return std_rollno;
+}
+public void setStd_rollno(int std_rollno) {
+	this.std_rollno = std_rollno;
+}
+public String getStd_name() {
+	return std_name;
+}
+public void setStd_name(String std_name) {
+	this.std_name = std_name;
+}
+public int getStd_marks() {
+	return std_marks;
+}
+public void setStd_marks(int std_marks) {
+	this.std_marks = std_marks;
+}
 
 }
