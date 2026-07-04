c5f79aa57c8fb87e50ac386b7b6e11a4fe96af7d
RKCodesTech <riyakosta2609@gmail.com>
2026-07-04 23:29:41 +0530

    s

--- StudentManagemen_Basic/src/main/java/in/sp/config/SpringConfigFile.java
+++ StudentManagemen_Basic/src/main/java/in/sp/config/SpringConfigFile.java
@@ -1,5 +1,10 @@
 package in.sp.config;
-@Config
-public class SpringConfigFile {
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfigFile {
+@Bean
+public 
 }
