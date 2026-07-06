a72d5f3d4ac064937179f2f7e752f615f6c2a53c
RKCodesTech <riyakosta2609@gmail.com>
2026-07-06 23:25:11 +0530

    ss

--- StudentManagemen_Basic/src/main/java/in/sp/dao/StudentDAO.java
+++ StudentManagemen_Basic/src/main/java/in/sp/dao/StudentDAO.java
@@ -13,4 +13,4 @@ public int insertStudent(Student student) {
 	String sql= "INSERT INTO STUDENT VALUES(?,?,?)";
 	return jdbcTemplate.update(sql,student.getStd_rollno() ,student.getStd_name(),student.getStd_marks());
 }
-}
+}                                      		
