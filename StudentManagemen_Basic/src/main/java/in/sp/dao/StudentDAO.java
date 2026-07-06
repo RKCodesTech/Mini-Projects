9ccc8b60836fc342f4d01176f1c135ec3e0a293a
RKCodesTech <riyakosta2609@gmail.com>
2026-07-07 00:51:55 +0530

    stddao

--- StudentManagemen_Basic/src/main/java/in/sp/dao/StudentDAO.java
+++ StudentManagemen_Basic/src/main/java/in/sp/dao/StudentDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
- 
+
 import in.sp.beans.Student;
 
 public class StudentDAO {
@@ -13,8 +13,8 @@ public class StudentDAO {
 
     public StudentDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-    } 
- 
+    }
+
     // Insert Student
     public int insertStudent(Student student) {
         String sql = "INSERT INTO student VALUES(?,?,?)";
@@ -24,41 +24,38 @@ public class StudentDAO {
                 student.getStd_marks());
     }
 
-
+    // Update Student
     public int updateStudent(Student student) {
-    	String sql="UPDATE student SET std_name=?,std_marks=?, WHERE std_rollno=?";
-    	return jdbcTemplate.update(sql,
-    			student.getStd_rollno(),
-    	student.getStd_name(),
-    	student.getStd_marks());
+        String sql = "UPDATE student SET std_name=?, std_marks=? WHERE std_rollno=?";
+        return jdbcTemplate.update(sql,
+                student.getStd_name(),
+                student.getStd_marks(),
+                student.getStd_rollno());
     }
+
     // Delete Student
     public int deleteStudent(int rollNo) {
         String sql = "DELETE FROM student WHERE std_rollno=?";
         return jdbcTemplate.update(sql, rollNo);
     }
 
-//     Search Student
+    // Search Student
     public Student searchStudent(int rollNo) {
         String sql = "SELECT * FROM student WHERE std_rollno=?";
         return jdbcTemplate.queryForObject(
                 sql,
-                new BeanPropertyRowMapper<>(Student.class),//it wil convert student row into obj and <> are generic that it will return op student
+                new BeanPropertyRowMapper<>(Student.class),
                 rollNo);
     }
 
-   public student searchStudent (int rollno) {
-	   String sql="SELECT * FROM student WHERE std_roll=?";
-	   return jdbcTemplate(sql,)
-   }
-    // Display All Students 
+    // Display All Students
     public List<Student> displayStudents() {
         String sql = "SELECT * FROM student";
         return jdbcTemplate.query(
                 sql,
                 new BeanPropertyRowMapper<>(Student.class));
     }
- 
+
     // Count Total Students
     public int totalStudents() {
         String sql = "SELECT COUNT(*) FROM student";
