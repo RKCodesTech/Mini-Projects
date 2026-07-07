4c54406aedc88d4d457aef37c2747d0fd5e6f455
RKCodesTech <riyakosta2609@gmail.com>
2026-07-07 19:13:55 +0530

    Student Services

--- StudentManagemen_Basic/src/main/java/in/sp/services/StudentServices.java
+++ StudentManagemen_Basic/src/main/java/in/sp/services/StudentServices.java
@@ -1,5 +1,23 @@
 package in.sp.services;
-
+import in.sp.dao.*;
+import in.sp.beans.*;
 public class StudentServices {
+	private StudentDAO studentdao;
+	public StudentServices(StudentDAO studentdao) {
+		this.studentdao=studentdao;
+	}
+	public int addStudent(Student student) {
+		return studentdao.insertStudent(student);
+		
+	}
+	public int updateStudent(Student student) {
+		return studentdao.updateStudent(student);
+		
+	}
+	public int deleteStudent(int rollno) {
+		return studentdao.deleteStudent(rollno);
+		
+	}
+	
 
 }
