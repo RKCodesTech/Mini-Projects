50cad5e880469abee20918df6057c62682f48673
RKCodesTech <riyakosta2609@gmail.com>
2026-07-08 23:24:23 +0530

    main.java

--- StudentManagemen_Basic/src/main/java/in/sp/main/Main.java
+++ StudentManagemen_Basic/src/main/java/in/sp/main/Main.java
@@ -2,14 +2,31 @@ package in.sp.main;
 import in.sp.services.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import java.util.Scanner;
 import in.sp.config.SpringConfigFile;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		int rollno;
+		String name;
+		int marks;
 	
 ApplicationContext context =new AnnotationConfigApplicationContext(SpringConfigFile.class);
 StudentServices services=context.getBean(StudentServices.class);
+while(true) {
+	System.out.println("To Add Student: write add" );
+	System.out.println("To View Students: write view");
+	System.out.println("To Search Students: write srch");
+	System.out.println("To Delete Students: write dlt");
+	System.out.println("To View Students: write view");
+	Scanner sc=new Scanner(System.in);
+	String choice=sc.nextLine();
+	switch(choice) {
+	case "add":
+		System.out.println("Enter Student name:");
+	
+		services.addStudent(null)
+	}
+}
 }
 }
